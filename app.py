a780255e340c568bf1a3d30792b834771f93105e
Shafin Ahmad <701ahmads@gmail.com>
2024-03-05 00:41:29 -0500

    Added flask paths for images, css and js

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') #Returns templates/index.html
 def index():
     return render_template('index.html')
+
+@app.route('/css/<css>') #Returns any file from css directory
+def giveCSS(css):
+    return send_from_directory('css', css)
+
+@app.route('/js/<js>') #Returns any file from js directory
+def giveJS(js):
+    return send_from_directory('js', js)
+
+@app.route('/images/<images>') #Returns any file from images directory
+def giveJS(images):
+    return send_from_directory('images', images)
 
 if __name__ == '__main__':
     app.run(debug=True)
