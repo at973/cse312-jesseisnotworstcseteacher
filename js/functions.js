bf91e154f20e45bb2b020dc2aa54cad90c6e11c9
Winston <panda501wp@gmail.com>
2024-03-24 20:15:48 -0400

    holy goddamn, that took way too long, but finally the textbox looks like it's aligned with the bottom of the screen, and everything looks okay

--- js/functions.js
+++ js/functions.js
@@ -3,7 +3,7 @@ function newChat(chatJSON) {
     const message = chatJSON.message;
     const id = chatJSON.id;
     const message_html = 
-        `<div class="tile is-vertical">
+        `<div class="tile is-vertical m-2 sent-message-box">
             <article class="message is-primary" id=${id}>
                 <div class="message-body"><b>${username}: </b> ${message} </div>
             </article>
@@ -31,6 +31,16 @@ function updateChat() {
     request.send();
 }
 
+function generateTestChats() {
+    chats = '';
+    for(let i = 0; i < 50; i++) {
+        chats += newChat({username: 'guest', message: 'this is just a test', id: i});
+    }
+    console.log('chats', chats);
+    document.getElementById('chatbox').innerHTML = chats;
+}
+
 function welcome() {
+    // generateTestChats();  // Line is only here for tests
     setInterval(updateChat, 5000);
 }
